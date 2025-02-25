5a25ee8c8ec98a0fb673cd4bc3b1b7da3c413434
Biagio Grassia <biagiograssia95@gmail.com>
2025-02-25 12:38:51 +0100

    aggiunti metodi create e delete

--- src/main/java/com/example/esercizio_native_query/controller/ProdottoController.java
+++ src/main/java/com/example/esercizio_native_query/controller/ProdottoController.java
@@ -1,13 +1,26 @@
 package com.example.esercizio_native_query.controller;
 
+import com.example.esercizio_native_query.entity.Prodotto;
 import com.example.esercizio_native_query.service.ProdottoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/prodotto")
 public class ProdottoController {
     @Autowired
     private ProdottoService prodottoService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Prodotto> createProdotto(@RequestBody Prodotto prodotto){
+        Prodotto creare = prodottoService.createProdotto(prodotto);
+        return ResponseEntity.ok(creare);
+
+    }
+
+    @DeleteMapping("/delete")
+    public Prodotto deleteProdotto(@RequestBody Prodotto prodotto){
+        return prodottoService.deleteProdotto(prodotto);
+    }
 }
--- src/main/java/com/example/esercizio_native_query/service/ProdottoService.java
+++ src/main/java/com/example/esercizio_native_query/service/ProdottoService.java
@@ -1,5 +1,6 @@
 package com.example.esercizio_native_query.service;
 
+import com.example.esercizio_native_query.entity.Prodotto;
 import com.example.esercizio_native_query.repository.ProdottoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,20 @@ import org.springframework.stereotype.Service;
 public class ProdottoService {
     @Autowired
     private ProdottoRepository prodottoRepository;
+
+    public Prodotto createProdotto(Prodotto prodotto){
+        return prodottoRepository.save(prodotto);
+    }
+
+    public Prodotto deleteProdotto(Prodotto prodotto){
+        prodottoRepository.delete(prodotto);
+        return prodotto;
+
+    }
+
+
+
+
+
+
 }
